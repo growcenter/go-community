459103c59c4ee3a92c0a95f73d2f93d4fc0809b6
Jeremy <jeremygprawira@gmail.com>
2024-08-04 00:33:56 +0700

    feat: GCA-XX | fix cors

--- internal/deliveries/http/middleware/cors.go
+++ internal/deliveries/http/middleware/cors.go
@@ -1,9 +1,7 @@
 package middleware
 
 import (
-	"fmt"
 	"go-community/internal/config"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -11,11 +9,11 @@ import (
 
 func (m *Middleware) corsMiddleware(config *config.Configuration) echo.MiddlewareFunc {
 	// origin := fmt.Sprintf("http://%s:%d", config.Frontend.Host, config.Frontend.Port)
-	origin := fmt.Sprintf("http://%s", config.Frontend.Host)
+	// origin := fmt.Sprintf("http://%s", config.Frontend.Host)
 
 	return middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{origin},
-		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPatch, http.MethodDelete, http.MethodOptions, http.MethodPut},
-		AllowHeaders: []string{echo.HeaderAccept, echo.HeaderAcceptEncoding, echo.HeaderContentType, echo.HeaderAccept, "X-API-Key", echo.HeaderAuthorization},
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"*"},
+		AllowHeaders: []string{"*"},
 	})
 }
