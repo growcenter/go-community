29e9cee8788296a8e06df3ef2de132fc4c4e86a7
Tobias Halomoan <agyasta1808@gmail.com>
2024-11-12 14:43:42 +0700

    Feat : Added Homebase Registration route.

--- internal/models/event_registration_model.go
+++ internal/models/event_registration_model.go
@@ -51,7 +51,16 @@ type (
 	CreateEventRegistrationRequest struct {
 		Name        string                                `json:"name" validate:"required,min=1,max=50" example:"Professionals"`
 		Identifier  string                                `json:"identifier" validate:"required,noStartEndSpaces,emailPhoneFormat"`
-		Address     string                                `json:"address" validate:"required,min=15,noStartEndSpaces"`
+		Address     string                                `json:"address,omitempty"`
+		EventCode   string                                `json:"eventCode" validate:"required,min=1,max=30,noStartEndSpaces" example:"Professionals"`
+		SessionCode string                                `json:"sessionCode" validate:"required,min=1,max=30,noStartEndSpaces" example:"Professionals"`
+		Others      []CreateOtherEventRegistrationRequest `json:"otherRegister" validate:"dive,required"`
+	}
+		CreateHomebaseRegistrationRequest struct {
+		Name        string                                `json:"name" validate:"required,min=1,max=50" example:"Professionals"`
+		Identifier  string                                `json:"identifier" validate:"required,noStartEndSpaces,emailPhoneFormat"`
+		AccountNumber string							  `json:"accountNumber" validate:"required,noStartEndSpaces"`
+		Address     string                                `json:"address,omitempty"`
 		EventCode   string                                `json:"eventCode" validate:"required,min=1,max=30,noStartEndSpaces" example:"Professionals"`
 		SessionCode string                                `json:"sessionCode" validate:"required,min=1,max=30,noStartEndSpaces" example:"Professionals"`
 		Others      []CreateOtherEventRegistrationRequest `json:"otherRegister" validate:"dive,required"`
