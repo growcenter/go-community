ed45bf67955e4fd345071d1356cb881e5bfb26a6
Jeremy Gerald Prawira <jeremy.prawira@amartha.com>
2025-09-10 23:17:39 +0700

    fix: cors for refresh token

--- internal/deliveries/http/v2/token_v2_handler.go
+++ internal/deliveries/http/v2/token_v2_handler.go
@@ -1,7 +1,6 @@
 package v2
 
 import (
-	"github.com/labstack/echo/v4"
 	"go-community/internal/common"
 	"go-community/internal/config"
 	"go-community/internal/constants"
@@ -11,6 +10,8 @@ import (
 	"go-community/internal/pkg/authorization"
 	"go-community/internal/usecases"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 type TokenHandler struct {
@@ -25,6 +26,7 @@ func NewTokenHandler(api *echo.Group, a *authorization.Auth, c *config.Configura
 	endpoint := api.Group("/tokens")
 	endpoint.Use(middleware.RefreshMiddleware(c))
 	endpoint.GET("", handler.Refresh)
+	endpoint.POST("", handler.RefreshPost)
 }
 
 // Refresh godoc
@@ -40,6 +42,46 @@ func NewTokenHandler(api *echo.Group, a *authorization.Auth, c *config.Configura
 // @Failure 422 {object} models.ErrorResponse{errors=models.ErrorValidateResponse} "Validation error. This can happen if there is an error validation while create account"
 // @Router /v2/tokens [get]
 func (th *TokenHandler) Refresh(ctx echo.Context) error {
+	id := ctx.Get("id").(string)
+	if common.IsValidUUID(id) {
+		tokens, err := th.auth.GenerateTokens(id, constants.TYPE_GUEST, constants.ROLE_GUEST)
+		if err != nil {
+			response.Error(ctx, err)
+		}
+
+		return response.SuccessList(ctx, http.StatusCreated, 2, tokens.ToGenerateTokens())
+	} else {
+		user, err := th.usecase.User.GetRBAC(ctx.Request().Context(), id)
+		if err != nil {
+			return response.Error(ctx, err)
+		}
+
+		if user == nil {
+			return response.Error(ctx, models.ErrorUserNotFound)
+		}
+
+		tokens, err := th.auth.GenerateTokens(user.CommunityId, user.Roles, user.UserTypes)
+		if err != nil {
+			response.Error(ctx, err)
+		}
+
+		return response.SuccessList(ctx, http.StatusCreated, 2, tokens.ToGenerateTokens())
+	}
+}
+
+// RefreshPost godoc
+// @Summary Generate Tokens using POST
+// @Description Generate both Access and Refresh Token using POST method
+// @Tags tokens
+// @Accept json
+// @Produce json
+// @Param Cookie path string true "object that needs to be added"
+// @Param X-API-Key header string true "mandatory header to access endpoint"
+// @Success 201 {object} models.List{data=[]models.TokensResponse} "Response indicates that the request succeeded and the new resources has been created and transmitted in the message body"
+// @Failure 400 {object} models.ErrorResponse "Bad Request"
+// @Failure 422 {object} models.ErrorResponse{errors=models.ErrorValidateResponse} "Validation error. This can happen if there is an error validation while create account"
+// @Router /v2/tokens [post]
+func (th *TokenHandler) RefreshPost(ctx echo.Context) error {
 	id := ctx.Get("id").(string)
 	if common.IsValidUUID(id) {
 		tokens, err := th.auth.GenerateTokens(id, constants.TYPE_GUEST, constants.ROLE_GUEST)
